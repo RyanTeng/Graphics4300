49f9faf258d1110ae5bc47fe554b02b7f1367f9a
Ryan Teng <ryanrteng@gmail.com>
2019-04-16 14:09:01 -0400

    Added shadows

--- ScenegraphBasicRaytracer/src/main/java/sgraph/RTScenegraphRenderer.java
+++ ScenegraphBasicRaytracer/src/main/java/sgraph/RTScenegraphRenderer.java
@@ -34,7 +34,7 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
     private Map<String, TextureImage> textures;
 
     public RTScenegraphRenderer() {
-        textures = new TreeMap<String,TextureImage>();
+        textures = new TreeMap<String, TextureImage>();
     }
 
     @Override
@@ -65,7 +65,7 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
 
     @Override
     public void draw(INode root, Stack<Matrix4f> modelView) {
-        int i,j;
+        int i, j;
         int width = 800;
         int height = 800;
         float FOVY = 120.0f;
@@ -73,13 +73,11 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
 
         this.lights = root.getLightsInView(modelView);
 
-        BufferedImage output = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+        BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        rayView.start = new Vector4f(0,0,0,1);
-        for (i=0;i<width;i++)
-        {
-            for (j=0;j<height;j++)
-            {
+        rayView.start = new Vector4f(0, 0, 0, 1);
+        for (i = 0; i < width; i++) {
+            for (j = 0; j < height; j++) {
                 /*
                  create ray in view coordinates
                  start point: 0,0,0 always!
@@ -89,25 +87,32 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
                  y = j-height/2
                  z = -0.5*height/tan(FOVY)
                 */
-                rayView.direction = new Vector4f(i-0.5f*width,
-                        j-0.5f*height,
-                        -0.5f*height/(float)Math.tan(Math.toRadians(0.5*FOVY)),
+                rayView.direction = new Vector4f(i - 0.5f * width,
+                        j - 0.5f * height,
+                        -0.5f * height / (float) Math.tan(Math.toRadians(0.5 * FOVY)),
                         0.0f);
+
                 HitRecord hitA = new HitRecord();
-                raycast(rayView,root,modelView,hitA);
-                Color colorA = new Color((int)(getRaytracedColor(hitA).getRGB() ));
-                Color colorR = new Color(0,0,0);
+                raycast(rayView, root, modelView, hitA);
+                Color colorA = new Color(getRaytracedColor(hitA, root, modelView).getRGB());
+
+                Color colorR = new Color(0, 0, 0);
                 if (hitA.material.getReflection() > 0) {
                     Ray reflectRay = new Ray();
                     reflectRay.start = hitA.point;
                     reflectRay.direction = rayView.direction.sub(hitA.normal.mul(2 * (hitA.normal.dot(rayView.direction))));
+                    reflectRay.direction.x *= ((0.8f + (float) Math.random() / 5));
+                    reflectRay.direction.y *= ((0.8f + (float) Math.random() / 5));
+                    reflectRay.direction.z *= ((0.8f + (float) Math.random() / 5));
                     HitRecord hitR = new HitRecord();
-                        raycast(reflectRay, root, modelView, hitR);
-                        colorR = new Color((int) (getRaytracedColor(hitR).getRGB()));
+                    raycast(reflectRay, root, modelView, hitR);
+                    if (hitR.intersected()) {
+                        colorR = new Color(getRaytracedColor(hitR, root, modelView).getRGB());
+                    }
                 }
                 Color color = blend(colorA, colorR, hitA.material.getAbsorption(), hitA.material.getReflection());
 
-                output.setRGB(i,height-1-j,color.getRGB());
+                output.setRGB(i, height - 1 - j, color.getRGB());
             }
         }
 
@@ -120,7 +125,7 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
         }
 
         try {
-            ImageIO.write(output,"png",outStream);
+            ImageIO.write(output, "png", outStream);
             outStream.close();
         } catch (IOException e) {
             throw new IllegalArgumentException("Could not write raytraced image!");
@@ -137,24 +142,23 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
         return new Color((int) r, (int) g, (int) b, (int) a);
     }
 
-    private void raycast(Ray rayView,INode root,Stack<Matrix4f> modelView,HitRecord hitRecord) {
-        root.intersect(rayView,modelView,hitRecord);
+    private void raycast(Ray rayView, INode root, Stack<Matrix4f> modelView, HitRecord hitRecord) {
+        root.intersect(rayView, modelView, hitRecord);
     }
 
-    private Color getRaytracedColor(HitRecord hitRecord) {
+    private Color getRaytracedColor(HitRecord hitRecord, INode root, Stack<Matrix4f> modelview) {
         if (hitRecord.intersected())
-            return shade(hitRecord.point,hitRecord.normal,hitRecord.material,
-                    hitRecord.textureName,hitRecord.texcoord);
+            return shade(hitRecord.point, hitRecord.normal, hitRecord.material,
+                    hitRecord.textureName, hitRecord.texcoord, root, modelview);
         else
-            return new Color(0,0,0);
+            return new Color(0, 0, 0);
     }
 
     private Color shade(Vector4f point, Vector4f normal, Material material,
-                        String textureName, Vector2f texcoord) {
-        Vector3f color = new Vector3f(0,0,0);
+                        String textureName, Vector2f texcoord, INode root, Stack<Matrix4f> modelview) {
+        Vector3f color = new Vector3f(0, 0, 0);
 
-        for (int i=0;i<lights.size();i++)
-        {
+        for (int i = 0; i < lights.size(); i++) {
             Vector3f lightVec;
             Vector3f spotdirection = new Vector3f(
                     lights.get(i).getSpotDirection().x,
@@ -162,41 +166,50 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
                     lights.get(i).getSpotDirection().z);
 
 
-            if (spotdirection.length()>0)
+            if (spotdirection.length() > 0)
                 spotdirection = spotdirection.normalize();
 
-            if (lights.get(i).getPosition().w!=0) {
+            if (lights.get(i).getPosition().w != 0) {
                 lightVec = new Vector3f(
                         lights.get(i).getPosition().x - point.x,
                         lights.get(i).getPosition().y - point.y,
                         lights.get(i).getPosition().z - point.z);
-            }
-            else
-            {
+            } else {
                 lightVec = new Vector3f(
                         -lights.get(i).getPosition().x,
                         -lights.get(i).getPosition().y,
                         -lights.get(i).getPosition().z);
             }
             lightVec = lightVec.normalize();
+            HitRecord hitS = new HitRecord();
+            Ray shadowRay = new Ray();
+            shadowRay.start = point;
+            shadowRay.direction = new Vector4f(lightVec, 0);
+            shadowRay.direction.x *= ((0.8f + (float) Math.random() / 5));
+            shadowRay.direction.y *= ((0.8f + (float) Math.random() / 5));
+            shadowRay.direction.z *= ((0.8f + (float) Math.random() / 5));
+            raycast(shadowRay, root, modelview, hitS);
 
+            if (hitS.intersected()) {
+                continue;
+            }
 
-        /* if point is not in the light cone of this light, move on to next light */
-            if (new Vector3f(lightVec).negate().dot(spotdirection)<=Math.cos(Math.toRadians(lights.get(i).getSpotCutoff())))
+            /* if point is not in the light cone of this light, move on to next light */
+            if (new Vector3f(lightVec).negate().dot(spotdirection) <= Math.cos(Math.toRadians(lights.get(i).getSpotCutoff())))
                 continue;
 
 
-            Vector3f normalView = new Vector3f(normal.x,normal.y,normal.z).normalize();
+            Vector3f normalView = new Vector3f(normal.x, normal.y, normal.z).normalize();
 
             float nDotL = normalView.dot(lightVec);
 
-            Vector3f viewVec = new Vector3f(point.x,point.y,point.z).negate();
+            Vector3f viewVec = new Vector3f(point.x, point.y, point.z).negate();
             viewVec = viewVec.normalize();
 
             Vector3f reflectVec = new Vector3f(lightVec).negate().reflect(normalView);
             reflectVec = reflectVec.normalize();
 
-            float rDotV = Math.max(reflectVec.dot(viewVec),0.0f);
+            float rDotV = Math.max(reflectVec.dot(viewVec), 0.0f);
 
             Vector3f ambient = new Vector3f(
                     material.getAmbient().x * lights.get(i).getAmbient().x,
@@ -204,19 +217,17 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
                     material.getAmbient().z * lights.get(i).getAmbient().z);
 
             Vector3f diffuse = new Vector3f(
-                    material.getDiffuse().x * lights.get(i).getDiffuse().x * Math.max(nDotL,0),
-                    material.getDiffuse().y * lights.get(i).getDiffuse().y * Math.max(nDotL,0),
-                    material.getDiffuse().z * lights.get(i).getDiffuse().z * Math.max(nDotL,0));
+                    material.getDiffuse().x * lights.get(i).getDiffuse().x * Math.max(nDotL, 0),
+                    material.getDiffuse().y * lights.get(i).getDiffuse().y * Math.max(nDotL, 0),
+                    material.getDiffuse().z * lights.get(i).getDiffuse().z * Math.max(nDotL, 0));
             Vector3f specular;
-            if (nDotL>0) {
+            if (nDotL > 0) {
                 specular = new Vector3f(
                         material.getSpecular().x * lights.get(i).getSpecular().x * (float) Math.pow(rDotV, material.getShininess()),
                         material.getSpecular().y * lights.get(i).getSpecular().y * (float) Math.pow(rDotV, material.getShininess()),
                         material.getSpecular().z * lights.get(i).getSpecular().z * (float) Math.pow(rDotV, material.getShininess()));
-            }
-            else
-            {
-                specular = new Vector3f(0,0,0);
+            } else {
+                specular = new Vector3f(0, 0, 0);
             }
             color = new Vector3f(color).add(ambient).add(diffuse).add(specular);
         }
@@ -226,11 +237,11 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
             color = color.mul(colorFromTexture.x, colorFromTexture.y, colorFromTexture.z);
         }
 
-        color.x = Math.min(color.x,1);
-        color.y = Math.min(color.y,1);
-        color.z = Math.min(color.z,1);
+        color.x = Math.min(color.x, 1);
+        color.y = Math.min(color.y, 1);
+        color.z = Math.min(color.z, 1);
 
-        return new Color((int)(255*color.x),(int)(255*color.y),(int)(255*color.z));
+        return new Color((int) (255 * color.x), (int) (255 * color.y), (int) (255 * color.z));
     }
 
     @Override
@@ -239,16 +250,15 @@ public class RTScenegraphRenderer implements IScenegraphRenderer {
     }
 
     @Override
-    public void addTexture(String name,String path)
-    {
+    public void addTexture(String name, String path) {
         TextureImage image = null;
-        String imageFormat = path.substring(path.indexOf('.')+1);
+        String imageFormat = path.substring(path.indexOf('.') + 1);
         try {
-            image = new TextureImage(path,imageFormat,name);
+            image = new TextureImage(path, imageFormat, name);
         } catch (IOException e) {
-            throw new IllegalArgumentException("Texture "+path+" cannot be read!");
+            throw new IllegalArgumentException("Texture " + path + " cannot be read!");
         }
-        textures.put(name,image);
+        textures.put(name, image);
     }
 
     @Override
